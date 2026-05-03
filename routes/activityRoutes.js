95bda9f04229f22e49458db72ff6307ee51ddd27
Harshini <harshiniravikumar2004@gmail.com>
2026-05-04 00:41:33 +0530

    fix

--- routes/activityRoutes.js
+++ routes/activityRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const auth   = require("../middleware/authMiddleware");
-const { getBoardActivity } = require("../controllers/activityController");
+const { getBoardActivity } = require("../controllers/ActivityController");
 
 router.get("/:boardId", auth, getBoardActivity);
 
